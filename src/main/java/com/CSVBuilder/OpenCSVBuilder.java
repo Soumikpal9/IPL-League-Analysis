89651d3d95b600fd29b5b003e313e5837681ec36
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-27 19:06:03 +0530

    [SP]Added: CSVBuilder Package Implemented

---
+++ src/main/java/com/CSVBuilder/OpenCSVBuilder.java
@@ -0,0 +1,36 @@
+package com.CSVBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.List;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class OpenCSVBuilder<E> implements ICSVBuilder {
+	@Override
+	public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVException {
+		try {
+			CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			CsvToBean<E> csvToBean = csvToBeanBuilder.withType(csvClass).withIgnoreLeadingWhiteSpace(true).build();
+			return csvToBean.iterator();
+		}
+		catch(IllegalStateException e) {
+			throw new CSVException(e.getMessage(), CSVException.ExceptionType.UNABLE_TO_PARSE);
+		}
+		//return this.getCSVBean(reader, csvClass).iterator();
+	}
+
+	@Override
+	public <E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CSVException {
+		try {
+			CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			CsvToBean<E> csvToBean = csvToBeanBuilder.withType(csvClass).withIgnoreLeadingWhiteSpace(true).build();
+			return csvToBean.parse();
+		}
+		catch(IllegalStateException e) {
+			throw new CSVException(e.getMessage(), CSVException.ExceptionType.UNABLE_TO_PARSE);
+		}
+		//return this.getCSVBean(reader, csvClass).parse();
+	}
+}
